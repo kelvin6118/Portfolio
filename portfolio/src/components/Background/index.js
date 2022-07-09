5c75dde038d83fbde094c9ea9313eb595288c6fb
Yan Wong <kelvinwong26546@gmail.com>
2022-07-09 19:11:30 +0800

    a bit background css

--- portfolio/src/components/Background/index.js
+++ portfolio/src/components/Background/index.js
@@ -2,19 +2,30 @@ import React from 'react';
 import background from "../../media/background-crop.gif"
 import './style.css';
 
-const Background = ({Content}) => {
+const Background = ({Content, Message}) => {
     return<div  style={{ backgroundImage: `url(${background})` }}>
         <div id="home-container">
-            <div id='background' style={{
+            <div style={{
                 width:"50%",
                 justifyContent: 'left',
+                display: 'flex',
+                flexDirection: 'column',
                 }}>
-                <h1 id="name">Kelvin Wong</h1>
-                <h2 id="title">Full-Stack Developer</h2>
+                    <div id='myself'>
+                        <h1 id="name">Kelvin Wong</h1>
+                        <h2 id="title">Full-Stack Developer</h2>
+                    </div>
+                    <div id='message'>
+                        {Message}
+                    </div>
                 </div>
+
             <div id='content' style={{
                 width:"50%"
-                }}>{Content}</div>
+                }}>
+                    {Content}
+                    </div>
+
         </div>
     </div>
 }
