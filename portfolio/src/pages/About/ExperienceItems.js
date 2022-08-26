ac3813cf5e2e7429e1a226e3f5d81360a3e1bc5e
Yan Wong <kelvinwong26546@gmail.com>
2022-08-26 20:40:56 +0800

    clean code my map experience

---
+++ portfolio/src/pages/About/ExperienceItems.js
@@ -0,0 +1,25 @@
+import FPlogo from "../../media/experiences/futureproof.png";
+import GPlogo from "../../media/experiences/grassroots.jpg";
+
+export const ExperienceItems = [
+    {
+        logo: FPlogo,
+        company: "Furureproof",
+        title: "Trainee Full-Stack Developer",
+        time: "Feb 2022 - current",
+        description: <p className='job-description'>
+            Trained by FutureProof through intensive courses. Learnt to be a fullstack software engineer. Front-end UI, API, Database, SQL, TDD methodology, agile development, version control, Docker etc.<br></br>
+            Throughout the training, we mainly use javascript and python programming language. They have given us many opportunities to experience a full project cycle in an agile environment, from planning to presentation. I have gained numerous knowledge of software infrastructure and how to be a full-stack engineer.</p>
+    },
+    {
+        company: "GrassrootsWeb",
+        logo: GPlogo,
+        title: "Volunteer web designer",
+        time: "Apr 2021 - Feb 2022",
+        description: <p className='job-description'>
+            Work as a volunteer web designer at Grassroots whos design and makes websites for different charity client. Experienced with getting client requirement, planning, teamwork, research and presentation. The tool we've use to make the website is by using Elementor.
+        </p>
+    }
+    
+
+]
--- portfolio/src/pages/About/index.js
+++ portfolio/src/pages/About/index.js
@@ -1,8 +1,6 @@
 import React, {useState} from 'react';
 import { Background, Experience_Card, HardSkill_Card, CubeCom, SoftSkill} from '../../components';
 import './style.css';
-import FPlogo from "../../media/experiences/futureproof.png";
-import GPlogo from "../../media/experiences/grassroots.jpg";
 import Reactlogo from "../../media/hardskills/react.png";
 import SQLlogo from "../../media/hardskills/postgresql.png";
 import HTMLlogo from "../../media/hardskills/html.png";
@@ -19,6 +17,7 @@ import Reduxlogo from "../../media/hardskills/redux.png"
 import Bootstraplogo from "../../media/hardskills/bootstrap.png"
 import Netlifylogo from "../../media/hardskills/netlify.png"
 import Herokulogo from "../../media/hardskills/heroku.png"
+import { ExperienceItems } from './ExperienceItems';
 
 const About = () => {
     const [information, setinformation] = useState();
@@ -28,24 +27,17 @@ const About = () => {
             case 'e':
                 return<>
                     <div id='experience-content'>
-                        <Experience_Card
-                        logo = {FPlogo}
-                        company = "Furureproof"
-                        title = "Trainee Full-Stack Developer"
-                        time = "Feb 2022 - current"
-                        description= {<p className='job-description'>
-                            Trained by FutureProof through intensive courses. Learnt to be a fullstack software engineer. Front-end UI, API, Database, SQL, TDD methodology, agile development, version control, Docker etc.<br></br>
-                            Throughout the training, we mainly use javascript and python programming language. They have given us many opportunities to experience a full project cycle in an agile environment, from planning to presentation. I have gained numerous knowledge of software infrastructure and how to be a full-stack engineer.</p>}
-                        />
-                        <Experience_Card
-                        company="GrassrootsWeb"
-                        logo = {GPlogo}
-                        title = "Volunteer web designer"
-                        time = "Apr 2021 - Feb 2022"
-                        description={<p className='job-description'>
-                            Work as a volunteer web designer at Grassroots whos design and makes websites for different charity client. Experienced with getting client requirement, planning, teamwork, research and presentation. The tool we've use to make the website is by using Elementor.
-                        </p>}
-                        />
+                        {ExperienceItems.map((item) => {
+                            return <Experience_Card
+                            logo = {item.logo}
+                            company = {item.company}
+                            title = {item.title}
+                            time = {item.time}
+                            description= {item.description}
+                            />
+                        })}
+                       
+                        
                     </div>
                 </>
             case 'h':
