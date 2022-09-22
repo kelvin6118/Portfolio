93dbec7a4117e29a2f5cfdb2bcb1220a3e360669
Yan Wong <kelvinwong26546@gmail.com>
2022-09-22 20:08:31 +0800

    add personal project page to portfolio

--- portfolio/src/App.js
+++ portfolio/src/App.js
@@ -1,7 +1,7 @@
 import { Route, Routes } from 'react-router-dom';
 import './App.css';
 import { NavBar } from './components';
-import { About, Contact, Home, FpProjects } from './pages';
+import { About, Contact, Home, FpProjects, PersonalProjects } from './pages';
 
 function App() {
   return (
@@ -12,6 +12,7 @@ function App() {
           <Route path='/' element={<Home/>}/>
           <Route path='/about' element={<About/>}/>
           <Route path='/futureproof_projects' element={<FpProjects/>}/>
+          <Route path='/personal_projects' element={<PersonalProjects/>}/>
           <Route path='/contact' element={<Contact/>}/>
         </Routes>
       </main>
---
+++ portfolio/src/pages/PersonalProjects/ProjectItems.js
@@ -0,0 +1,13 @@
+import anonymous_diary from '../../media/Personal_Projects/Anonymous_Diary.PNG'
+import link from '../../media/contact/link.png'
+
+export const ProjectItems = [
+    {
+        appName:"Anonymous Diary",
+        screenShot:anonymous_diary,
+        description:<p className='project_description'>An application that allows people post their dairy anonymously</p>,
+        github:"https://github.com/kelvin6118/Anonymous_Diary_Client",
+        link:"https://kelvin-anonymous-diary.netlify.app/",
+        logo:link
+    }
+]
