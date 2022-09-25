9d4bd0051fb6ecda80b5925074271d597fe7effd
Yan Wong <kelvinwong26546@gmail.com>
2022-09-26 02:50:20 +0800

    refactor project file structure

--- portfolio/src/pages/index.js
+++ portfolio/src/pages/index.js
@@ -1,5 +1,4 @@
 export{default as Home} from './Home';
 export{default as About} from './About';
 export{default as Contact} from './Contact';
-export{default as FpProjects} from './FpProjects';
-export{default as PersonalProjects} from './PersonalProjects';
+export{default as Projects} from './Projects';
