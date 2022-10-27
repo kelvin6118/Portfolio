07e6f2c01192930bca6d65557bd8e111c0211079
Yan Wong <kelvinwong26546@gmail.com>
2022-10-27 19:22:23 +0800

    change home style

--- portfolio/src/pages/Home/index.js
+++ portfolio/src/pages/Home/index.js
@@ -19,20 +19,19 @@ const Content = () => {
     </>*/
 }
 
-const Message = () => {
-    return<>
-        <p>
-            Hi, Welcome to my portfolio<br></br>
-            At here, you can know more about me and have a look at some of my work.<br></br>
-            <br></br>
-            If you are interested about me and want to give me some opportunity to work as a developer, feel free to go to the contact pages and drop me a message.
-        </p>
-    </>
-}
-
 const Home = () => {
-    return<>
-    <Background Content={Content()} Message={Message()}/>
-    </>
+    return<main id='home-container'>
+            <header id='home-header'>
+                <h1 id="name">Kelvin Wong</h1>
+                <h2 id="title">Full-Stack Developer</h2>
+            </header>
+            <article className='message'>
+                <p>
+                Hi, Welcome to my portfolio<br/>
+                At here, you can know more about me and have a look at some of my work.<br/>
+                If you are interested about me and want to give me some opportunity to work as a developer, feel free to go to the contact pages and drop me a message.
+                </p>
+            </article>
+    </main>
 }
 export default Home;
